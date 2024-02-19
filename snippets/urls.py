115baa3ab650bf16bea1c99259fce01b9ce2d2e3
Rhythm1821 <alamrawat1821@gmail.com>
2024-02-19 20:53:12 +0530

    Authentication part-1

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,8 +3,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('',views.SnippetList.as_view()),
-    path('<int:pk>/',views.SnippetDetail.as_view())
+    path('',views.UserList.as_view()),
+    path('<int:pk>/',views.UserDetail.as_view()),
+    # path()
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse,Http404
+from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status,mixins,generics
 from rest_framework.views import APIView
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer,UserSerializer
 
 # class based views using generics
 class SnippetList(generics.ListCreateAPIView):
@@ -15,6 +16,15 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = UserSerializer
+    
 
 # Class based views using mixins
 # class SnippetList(mixins.ListModelMixin,
